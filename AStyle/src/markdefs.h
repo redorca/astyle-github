3c1c6788648f1da2e3ec40451411449346a7bd08
Bill <bill@zglue.com>
2019-02-21 16:16:07 -0800

    Move to DOS line endings.
    Change-Id: Ife87c1a6ce5b34d6dd960f0cb837c356adb99538

--- AStyle/src/markdefs.h
+++ AStyle/src/markdefs.h
@@ -1,4 +1,8 @@
 
+// #define GEN0
+#define GEN1
+
+#if defined(GEN0)
 #if defined(VERBOSE)
 static  void MARK_EXIT(const char *caller) { printf("==^^^^ %s :\n", caller); }
 static  void MARK_ENTRY(const char *caller) { printf("==vvvv %s :\n", caller); }
@@ -6,3 +10,13 @@ static  void MARK_ENTRY(const char *caller) { printf("==vvvv %s :\n", caller); }
 static  void MARK_EXIT(const char *caller) {  const char *tmp; tmp = caller;}
 static  void MARK_ENTRY(const char *caller) {  const char *tmp; tmp = caller;}
 #endif
+#endif /* GEN0 */
+
+extern char spaces[];
+extern int spacing;
+#if defined(GEN1)
+#define MARK_EXIT() { printf("^^^====%s%s\n", &spaces[spacing], __FUNCTION__); spacing--; }
+#define MARK_ENTRY() { spacing++; printf("vvv====%s%s\n", &spaces[spacing], __FUNCTION__); }
+
+#endif /* GEN1 */
+
