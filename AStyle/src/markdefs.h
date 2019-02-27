1209350bd424d0a5e924e4b9ab28faf826599f45
Bill <bill@zglue.com>
2019-02-26 20:37:28 -0800

    Clean up but not tested.
    Change-Id: I786b35b797b3eb852863f70a82b8282fb3bc9265

--- AStyle/src/markdefs.h
+++ AStyle/src/markdefs.h
@@ -1,22 +1,9 @@
 
-// #define GEN0
-#define GEN1
-
-#if defined(GEN0)
-#if defined(VERBOSE)
-static  void MARK_EXIT(const char *caller) { printf("==^^^^ %s :\n", caller); }
-static  void MARK_ENTRY(const char *caller) { printf("==vvvv %s :\n", caller); }
-#else
-static  void MARK_EXIT(const char *caller) {  const char *tmp; tmp = caller;}
-static  void MARK_ENTRY(const char *caller) {  const char *tmp; tmp = caller;}
-#endif
-#endif /* GEN0 */
 
 extern char depth_indent[];
 extern int depth;
 #define MAX_INDENT 60
 
-#if defined(GEN1)
 #define DEPTH   (MAX_INDENT - (depth % MAX_INDENT))
 #define NEW_DEPTH(a)    depth += a
 #define EXIT    "^^^"
@@ -28,34 +15,14 @@ extern int depth;
                 }
 #define STEP  2
 
-#if !defined(OLD_MARKERS)
 #define MARK_EXIT()    \
-                {                       \
-                        LABEL(EXIT)     \
-                        NEW_DEPTH(-STEP);  \
+                {                               \
+                        LABEL(EXIT)             \
+                        NEW_DEPTH(-STEP);       \
                 }
 #define MARK_ENTRY()    \
-                {                       \
-                        NEW_DEPTH(STEP);   \
-                        LABEL(ENTER)    \
+                {                               \
+                        NEW_DEPTH(STEP);        \
+                        LABEL(ENTER)            \
                 }
-#else
-#define MARK_EXIT()     LABEL(EXIT)
-#define MARK_ENTRY()    LABEL(ENTER)
-#endif
-
-#if defined(OLD_MARKERS)
-#define MARK_EXIT()                     \
-                {                                                                                       \
-                        printf("^^^::%d ====%s%s\n", DEPTH, &depth_indent[DEPTH], __FUNCTION__);        \
-                        NEW_DEPTH;                                                                      \
-                }
-#define MARK_ENTRY() \
-                {
-                        printf("vvv::%d ====%s%s\n", DEPTH, &depth_indent[DEPTH], __FUNCTION__);        \
-                        NEW_DEPTH;                                                                      \
-                }
-#endif /* OLD_MARKERS */
-
-#endif /* GEN1 */
 
