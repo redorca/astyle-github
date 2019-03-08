2e2f2cb195777848c51446a9dc371d6cb547ef50
Bill <bill@zglue.com>
2019-03-08 15:58:56 -0800

    Add null debug output feature
    Change-Id: I4deb56f5a31f8028f8bb591a99a10f26883ed1c3

--- AStyle/src/markdefs.h
+++ AStyle/src/markdefs.h
@@ -4,29 +4,31 @@ extern char depth_indent[];
 extern int depth;
 #define MAX_INDENT 60
 
-#define SHOW_LINE(a)     \
-        printf("%s :: %s\n", __FUNCTION__, a);
-
 #define DEPTH   (MAX_INDENT - (depth % MAX_INDENT))
 #define NEW_DEPTH(a)    depth += a
 #define EXIT    "^^^"
 #define ENTER   "vvv"
+#define STEP  2
+
+#if defined(ACTIVE)
+
+#define SHOW_LINE(a)     \
+        printf("%s :: %s\n", __FUNCTION__, a);
 
 #define LABEL(a)                         \
                 {                                                                                       \
                         printf("%s::%d ==== %s%s\n", a, DEPTH, &depth_indent[DEPTH], __FUNCTION__);    \
                 }
-#define STEP  2
 
 #define MARK_EXIT()    \
                 {                               \
-                        LABEL(EXIT)             \
+                        LABEL(EXIT);            \
                         NEW_DEPTH(-STEP);       \
                 }
 #define MARK_ENTRY()    \
                 {                               \
                         NEW_DEPTH(STEP);        \
-                        LABEL(ENTER)            \
+                        LABEL(ENTER);           \
                 }
 
 #define RETURN(args ...)   \
@@ -34,5 +36,14 @@ extern int depth;
                 MARK_EXIT();    \
                 return args;    \
         }
+#else
 
+#define SHOW_LINE(a)
 
+#define SHOW_DEPTH(XXX)
+#define LABEL(a)
+#define MARK_EXIT()
+#define MARK_ENTRY()
+#define RETURN(args ...)   return args
+
+#endif
